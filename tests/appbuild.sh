96168111d75e4d24aad5c311c74baf01744cb2f4
Dom Heinzeller <dom.heinzeller@noaa.gov>
2019-06-04 10:30:08 -0600

    conf/before_components.mk, tests/appbuild.sh: remove logic to replace default modulefiles for Theia/Gaea with CCPP versions for Intel15/16

--- tests/appbuild.sh
+++ tests/appbuild.sh
@@ -23,20 +23,9 @@ rm -f $PATHTR/../NEMS/exe/NEMS.x
 rm -f $PATHTR/../NEMS/src/conf/modules.nems
 
 # Replicate logic in conf/before_components
-if [[ $APP = CCPP_static_trans ]]; then
-  if [[ $MACHINE_ID = theia.intel ]]; then
-    echo "Move original modulefile modulefiles/theia.intel/fv3 aside and replace with modulefiles/theia.intel/fv3.intel-15.1.133"
-    cd modulefiles/theia.intel
-    mv -v fv3 fv3.original
-    ln -svf fv3.intel-15.1.133 fv3
-    cd ../..
-  else
-    echo "ERROR, appbuild.sh with APP CCPP_static_trans not configured for build target ${MACHINE_ID}"
-    exit 1
-  fi
 # DH* temporary, use second line once standaloneFV3 is using Intel18 if this logic is still required
-elif [[ $APP = CCPP || $APP = CCPP_repro || $APP = standaloneFV3_repro || $APP = standaloneFV3_intel18 ]]; then
-#elif [[ $APP = CCPP || $APP = CCPP_repro || $APP = standaloneFV3_repro || $APP = standaloneFV3 ]]; then
+if [[ $APP = CCPP || $APP = CCPP_repro || $APP = standaloneFV3_repro || $APP = standaloneFV3_intel18 ]]; then
+#if [[ $APP = CCPP || $APP = CCPP_repro || $APP = standaloneFV3_repro || $APP = standaloneFV3 ]]; then
 # *DH
   if [[ $MACHINE_ID = theia.intel ]]; then
     echo "Move original modulefile modulefiles/theia.intel/fv3 aside and replace with modulefiles/theia.intel/fv3.intel-18.0.1.163"
@@ -58,8 +47,8 @@ cd ${PATHTR}/..
 
 # Revert replicating logic in conf/before_components
 # DH* temporary, use second line once standaloneFV3 is using Intel18 if this logic is still required
-if [[ $APP = CCPP || $APP = CCPP_static_trans || $APP = CCPP_repro || $APP = standaloneFV3_repro || $APP = standaloneFV3_intel18 ]]; then
-#if [[ $APP = CCPP || $APP = CCPP_static_trans || $APP = CCPP_repro || $APP = standaloneFV3_repro || $APP = standaloneFV3 ]]; then
+if [[ $APP = CCPP || $APP = CCPP_repro || $APP = standaloneFV3_repro || $APP = standaloneFV3_intel18 ]]; then
+#if [[ $APP = CCPP || $APP = CCPP_repro || $APP = standaloneFV3_repro || $APP = standaloneFV3 ]]; then
 # *DH
   if [[ $MACHINE_ID = theia.intel ]]; then
     echo "Reinstantiate original modulefile modulefiles/theia.intel/fv3"
